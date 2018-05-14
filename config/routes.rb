ef3a90dcfc7bc145bd46b2df2debce9de24d243d
Ofir Dahan <ofirdahan112@gmail.com>
2018-05-14 13:30:39 -0700

    added routes to support crypto and stock searches

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get '/my-portfolio', to: 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
+  get 'search_crypto', to: 'cryptos#search'
 end
