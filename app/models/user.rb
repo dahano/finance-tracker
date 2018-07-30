55060c2a22214998eb0634df22566604aa802ba5
Ofir Dahan <ofirdahan112@gmail.com>
2018-07-29 17:14:43 -0700

    added friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :stocks, through: :user_stocks
   has_many :user_crypto
   has_many :cryptos, through: :user_crypto
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   def full_name
     return "#{first_name} #{last_name}".strip if first_name || last_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_034747) do
+ActiveRecord::Schema.define(version: 2018_07_30_001200) do
 
   create_table "cryptos", force: :cascade do |t|
     t.string "bitcurrency"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2018_07_11_034747) do
     t.decimal "last"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "stocks", force: :cascade do |t|
